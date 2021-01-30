e78ac0d74029c91de927bd465430c18f6b10260e
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-01-30 14:01:03 +0000

    fetching api, rendering data to dom

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-const App = () => {
+
+
+const fact = ({fact}) => {
   return (
-    <div>Hello</div>
+    <div>
+      <p>{fact}</p>
+    </div>
   )
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      facts: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://cat-fact.herokuapp.com/facts/random?amount=2')
+    .then(response => response.json())
+    .then(result => {
+      this.setState({
+        facts: result,
+        isLoaded: true
+      })
+    })
+  }
+
+  render() {
+    
+  const {isLoaded, facts} = this.state
+    
+   if (!isLoaded){
+     return <div>loading</div>
+   } else {
+     return facts.map((item, index) => <p key={index}>{item.text}</p>)
+   }
+  }
 }
 
 
